b0245f7ddf7b5c7a8f3e5b4400e00d79d37b0558
Trung Tran <trung.tran@duke.edu>
2022-04-26 11:37:41 -0400

    Update script for submitting bam files

--- scripts/submit_data.sh
+++ scripts/submit_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+declare -a BAMFILES=("/usr/xtmp/tqtran/data/cd/mnase/DM498_MNase_rep1_0_min.bam")
 
+# Iterate the string array using for loop
+for BAM in ${BAMFILES[@]}; do
+    sbatch -D ./slurm-logs/ --job-name="cd_data" --export="PYFILE=src/vit_data.py,ARGS=$BAM" scripts/cpu_job.sh
+done
 
-
-BAM=
-
-sbatch -D ./slurm-logs/ --job-name="cd_data" --export="PYFILE=src/vit_data.py,ARGS=$BAM" scripts/cpu_job.sh
