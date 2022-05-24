555bc3a8e2580bcea7f0bcbd1a4d8a2bff768180
Trung Tran <trung.tran@duke.edu>
2022-05-24 15:08:51 -0400

    Create complex configs

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-NUM_RUNS=1
+NUM_RUNS=5
 
 for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=v_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=simple" \
+    sbatch -D ./slurm-logs/ --job-name=c_$RUN \
+        --export="PYFILE=src/vit_train.py,ARGS=complex" \
         scripts/gpu_job.sh
 done
