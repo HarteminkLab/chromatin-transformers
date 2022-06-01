0c224f296286ee27be7ec66f91a37c5c368b95ae
Trung Tran <trung.tran@duke.edu>
2022-06-01 16:48:54 -0400

    Fix job names

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -3,16 +3,16 @@
 NUM_RUNS=5
 
 # Data using merged replicates
-for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=24x_merged_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=24x_120_merged" \
-        scripts/gpu_job.sh
-done
+#for RUN in `seq $NUM_RUNS`; do 
+#    sbatch -D ./slurm-logs/ --job-name=24x_merged_$RUN \
+#        --export="PYFILE=src/vit_train.py,ARGS=24x_120_merged" \
+#        scripts/gpu_job.sh
+#done
 
 
 # Channel for each replicate
 for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=24x_merged_$RUN \
+    sbatch -D ./slurm-logs/ --job-name=24x_repl_$RUN \
         --export="PYFILE=src/vit_train.py,ARGS=24x_120_replicates" \
         scripts/gpu_job.sh
 done
