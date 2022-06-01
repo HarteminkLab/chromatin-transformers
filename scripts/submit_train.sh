477ebf261859d0cbc7f7086db9a9b97450f63efc
Trung Tran <trung.tran@duke.edu>
2022-06-01 16:50:44 -0400

    Submit 12x64 jobs

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -11,8 +11,15 @@ NUM_RUNS=5
 
 
 # Channel for each replicate
+#for RUN in `seq $NUM_RUNS`; do 
+#    sbatch -D ./slurm-logs/ --job-name=24x_repl_$RUN \
+#        --export="PYFILE=src/vit_train.py,ARGS=24x_120_replicates" \
+#        scripts/gpu_job.sh
+#done
+
+# Channel for each replicate
 for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=24x_repl_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=24x_120_replicates" \
+    sbatch -D ./slurm-logs/ --job-name=12x_merge_$RUN \
+        --export="PYFILE=src/vit_train.py,ARGS=12x_120_merged" \
         scripts/gpu_job.sh
 done
