a673ae892625ca944cc0d8a8adebca2d8b9e5d02
Trung Tran <trung.tran@duke.edu>
2022-06-07 11:20:43 -0400

    chrom validation set jobs

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -5,6 +5,6 @@ NUM_RUNS=5
 # Cell cycle run
 for RUN in `seq $NUM_RUNS`; do 
     sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_simple" \
+        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom" \
         scripts/gpu_job.sh
 done
