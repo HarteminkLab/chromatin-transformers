a6c74a1fb0e1d640aa19a1b17f5952e1da4dce29
Trung Tran <trung.tran@duke.edu>
2022-06-07 14:15:56 -0400

    Add simple chr4 batch 4 size job

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -8,3 +8,13 @@ for RUN in `seq $NUM_RUNS`; do
         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b4" \
         scripts/gpu_job.sh
 done
+
+
+NUM_RUNS=5
+# Cell cycle run
+for RUN in `seq $NUM_RUNS`; do 
+    sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
+        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b4_simple" \
+        scripts/gpu_job.sh
+done
+
