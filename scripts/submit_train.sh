61249a3cf76267647d81f53ed2ecd7b40c37093e
Trung Tran <trung.tran@duke.edu>
2022-06-07 18:13:19 -0400

    Try increased drop out for overfitting

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -4,24 +4,6 @@ NUM_RUNS=2
 
 for RUN in `seq $NUM_RUNS`; do 
     sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b4" \
-        scripts/gpu_job.sh
-done
-
-for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b4_simple" \
-        scripts/gpu_job.sh
-done
-
-for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b32_simple" \
-        scripts/gpu_job.sh
-done
-
-for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=cc_simple_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b96_simple" \
+        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_chr4_validchrom_b96_simple_p20" \
         scripts/gpu_job.sh
 done
