fd1afda157900e15facb98ed8229c68c3e0345b4
Trung Tran <trung.tran@duke.edu>
2022-06-10 10:53:22 -0400

    Cadmium random ORFs

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -2,14 +2,20 @@
 
 NUM_RUNS=2
 
-for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_baseline" \
-        scripts/gpu_job.sh
-done
+# for RUN in `seq $NUM_RUNS`; do 
+#     sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
+#         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_baseline" \
+#         scripts/gpu_job.sh
+# done
+
+# for RUN in `seq $NUM_RUNS`; do 
+#     sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
+#         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_simple" \
+#         scripts/gpu_job.sh
+# done
 
 for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
-        --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_simple" \
+    sbatch -D ./slurm-logs/ --job-name=cd_$RUN \
+        --export="PYFILE=src/vit_train.py,ARGS=cadmium_24x_random_simple" \
         scripts/gpu_job.sh
 done
