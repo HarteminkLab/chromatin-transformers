07504162e7feb1bf357879fc3496381ff6e5b26d
Trung Tran <trung.tran@duke.edu>
2022-06-13 13:42:42 -0400

    Training examples

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -2,32 +2,14 @@
 
 NUM_RUNS=2
 
-# for RUN in `seq $NUM_RUNS`; do 
-#     sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
-#         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_baseline" \
-#         scripts/gpu_job.sh
-# done
-
-# for RUN in `seq $NUM_RUNS`; do 
-#     sbatch -D ./slurm-logs/ --job-name=t50_$RUN \
-#         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_random_simple" \
-#         scripts/gpu_job.sh
-# done
-
-# for RUN in `seq $NUM_RUNS`; do 
-#     sbatch -D ./slurm-logs/ --job-name=cd_$RUN \
-#         --export="PYFILE=src/vit_train.py,ARGS=cadmium_24x_random_simple" \
-#         scripts/gpu_job.sh
-# done
-
-#for RUN in `seq $NUM_RUNS`; do 
-#    sbatch -D ./slurm-logs/ --job-name=cdh_$RUN \
-#        --export="PYFILE=src/vit_train.py,ARGS=cadmium_24x_hybrid_simple" \
-#        scripts/gpu_job.sh
-#done
+for RUN in `seq $NUM_RUNS`; do 
+   sbatch -D ./slurm-logs/ --job-name=cdh_$RUN \
+       --export="PYFILE=src/vit_train.py,ARGS=cadmium_24x_hybrid_simple" \
+       scripts/gpu_job.sh
+done
 
 for RUN in `seq $NUM_RUNS`; do 
-    sbatch -D ./slurm-logs/ --job-name=cdh_$RUN \
+    sbatch -D ./slurm-logs/ --job-name=cch_$RUN \
         --export="PYFILE=src/vit_train.py,ARGS=cell_cycle_24x_hybrid_simple" \
         scripts/gpu_job.sh
 done
