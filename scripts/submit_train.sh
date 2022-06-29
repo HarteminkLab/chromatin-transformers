33cd93e1204a09c9bd5797c6b87534a82660f937
Trung Tran <trung.tran@duke.edu>
2022-06-29 09:08:34 -0400

    More complex cd run

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -4,12 +4,6 @@ NUM_RUNS=5
 
 for RUN in `seq $NUM_RUNS`; do 
    sbatch -D ./slurm-logs/ --job-name=cd_$RUN \
-       --export="PYFILE=src/vit_train.py,ARGS=cd_24x_complex" \
-       scripts/gpu_job.sh
-done
-
-for RUN in `seq $NUM_RUNS`; do 
-   sbatch -D ./slurm-logs/ --job-name=cd_$RUN \
-       --export="PYFILE=src/vit_train.py,ARGS=cd_24x_more_complex" \
+       --export="PYFILE=src/vit_train.py,ARGS=cd_24x_even_more_complex" \
        scripts/gpu_job.sh
 done
