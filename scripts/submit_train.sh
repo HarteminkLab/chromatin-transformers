52d760512ead400fa8bf095ea9bf1620e982b774
Trung Tran <trung.tran@duke.edu>
2022-07-31 11:18:49 -0400

    Update submit train

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -11,3 +11,4 @@ sbatch -D ./slurm-logs/ --job-name=lr_$RUN \
 sbatch -D ./slurm-logs/ --job-name=lr_$RUN \
     --export="PYFILE=src/vit_train.py,ARGS=resume output/cd_24x128_chr2_long_running_20220715_cb94" \
     scripts/gpu_job.sh
+
